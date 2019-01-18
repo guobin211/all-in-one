5da7eba3727c2f50c7c80ac45d8795500e7d66c9
eloven <guobin201314@gmail.com>
2019-01-18 14:15:28 +0800

    静态资源

--- app-ionic/src/app/app.component.ts
+++ app-ionic/src/app/app.component.ts
@@ -3,28 +3,55 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { HomePage } from '../components/home/home';
+import { ListComponent } from '../components/list/list';
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string, path: string}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  childComponent: Array<{title: string, Component: any}>;
+
+  constructor(public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
+      { title: 'Home', path: '' },
+      { title: 'List', path: 'page-list' },
     ];
-
   }
 
   initializeApp() {
@@ -39,6 +66,11 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    if (page % 2 === 0) {
+      this.nav.setRoot(HomePage);
+    } else {
+      this.nav.setRoot(ListComponent);
+    }
+
   }
 }
--- app-ionic/src/app/app.module.ts
+++ app-ionic/src/app/app.module.ts
@@ -1,39 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { HomePage } from '../components/home/home';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { Camera } from '@ionic-native/camera';
+import { ListComponent } from '../components/list/list';
 
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage,
-
+    ListComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp, {
+      monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+      monthShortNames: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+      dayNames: ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+      dayShortNames: ['7', '1', '2', '3', '4', '5', '6'],
+    }),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage,
-
+    ListComponent,
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
     { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
